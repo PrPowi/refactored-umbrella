b03dafd0826d69881f43bbb917c992170ed193dd
PrPowi <140035825+PrPowi@users.noreply.github.com>
2023-07-22 19:31:38 +0800

    Initial commit

---
+++ 源.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define x 3.14
+int main() {
+    /*int r, h;
+    scanf("%d %d", &r, &h);
+    int y = r + h;
+    printf("%d", y);
+    return 0;*/
+   /* for (int i = 0; i < 5; i++)
+        printf("hello-----bite\n");
+        return 0;*/
+    
+}
